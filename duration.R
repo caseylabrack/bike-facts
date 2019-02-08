dd4eb7ab0e8b785b0199c256e2896d3962628858
Casey Labrack <clabrack@gmail.com>
2019-02-08 18:07:23 -0500

    update title for ride time

--- duration.R
+++ duration.R
@@ -11,7 +11,7 @@ ggplot(data, aes(duration)) +
            lineheight = 1, size = rel(2)) +
   scale_y_continuous(expand = c(0,0)) +
   scale_x_continuous(limits = c(60,60*60), breaks = c(60,seq(0,60*60, 60*15)), labels = function (x) { x / 60}, expand = c(0,0)) +
-  labs(title = "Average Ride Time", subtitle = "Distribution of Ride Times in Minutes") +
+  labs(title = "Bikeshare Trips Are Short", subtitle = "Distribution of Ride Times in Minutes") +
   theme(axis.title = element_blank(),
         axis.text.y = element_blank(),
         axis.ticks = element_blank(),
