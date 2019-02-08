f5d01c07622d8ac8d1f83a9a312110fef9d5283f
Casey Labrack <clabrack@gmail.com>
2019-02-08 17:45:41 -0500

    title/caption change to time of day chart

--- time-of-day.R
+++ time-of-day.R
@@ -7,15 +7,15 @@ data = read_csv("201812-capitalbikeshare-tripdata.csv") %>%
 
 ggplot(data, aes(time)) +
   geom_bar(fill = "#d79544") +
-  scale_x_continuous(breaks = c(9,12,17), labels = c("9am", "noon", "5pm"), name ="", expand = c(0,0)) +
+  scale_x_continuous(breaks = c(9,12,17), labels = c("9am", "noon", "5pm"), expand = c(0,0)) +
   scale_y_continuous(expand = c(0,0)) +
-  labs(title = "When People Are Riding Bikeshare", caption = "Bikeshare use peaks around commuting hours.") +
+  labs(title = "People Use Bikeshare to Commute", subtitle = "Rides by hour of the day") +
   theme(axis.text.y = element_blank(),
-        axis.ticks.y = element_blank(),
-        axis.title.y = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title = element_blank(),
         panel.background = element_blank(),
-        plot.caption = element_text(hjust = 0),
-        plot.title = element_text(margin = margin(t = 0, r = 0, b = 24, l = 0, unit = "pt")))
+        plot.subtitle = element_text(hjust = 0, face = "italic",
+                                    margin = margin(t = 0, r = 0, b = 24, l = 0, unit = "pt")))
 
 ggsave("time-of-day.pdf", width = 5.5, height = 2.75, unit = "in")
 ggsave("time-of-day.png", width = 5.5, height = 2.75, dpi = 300, unit = "in")
